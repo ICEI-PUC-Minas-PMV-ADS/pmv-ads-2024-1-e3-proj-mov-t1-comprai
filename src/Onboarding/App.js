fcef6518a5e79b37c77522e8bec6b39e2f1c3180
Mahavishnu Jn <mahajneesh@hotmail.com>
2024-04-06 13:33:05 -0300

    Revert "Revert "Revert "Revert "Merge branch 'main' of https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2024-1-e3-proj-mov-t1-comprai""""
    This reverts commit 280e089119627709169d4e1f5bff11c1ff525c4f.

---
+++ src/Onboarding/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import Onboarding from './pages/Onboarding'
+
+export default function App() {
+  return (
+    <Onboarding />
+  );
+}
