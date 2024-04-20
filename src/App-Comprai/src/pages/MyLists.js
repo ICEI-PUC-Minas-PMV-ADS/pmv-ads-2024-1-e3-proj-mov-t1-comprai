6cca3351777c7d331bca958a3334cd4d26121266
Pedro-HFC <1457131@sga.pucminas.br>
2024-04-20 19:15:13 -0300

    PaginasMyListsLeituraJsonApresentacaoEmTela

--- src/App-Comprai/src/pages/MyLists.js
+++ src/App-Comprai/src/pages/MyLists.js
@@ -1,18 +1,53 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import CustomButton from '../components/CustomButton';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import CustomButton from "../components/CustomButton";
+import jsonData from "../components/listas.json";
 
-export default function MyLists({navigation}) {
-
+export default function MyLists({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.title}>Página Minhas Listas</Text>
+        <FlatList
+          data={jsonData}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => handleListItemPress(item)}>
+              <View style={styles.itemContainer}>
+                <Text style={styles.itemText}>{item.nome}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          contentContainerStyle={styles.listContent}
+        />
+
         <View style={styles.positionButton}>
-          <CustomButton title={'Nova Lista'} icon={'add'} onPress={() => { navigation.navigate('List') }} />
+          <CustomButton
+            title={"Nova Lista"}
+            icon={"add"}
+            onPress={() => {
+              navigation.navigate("List");
+            }}
+          />
         </View>
       </View>
-    </SafeAreaView >
+    </SafeAreaView>
   );
+  const handleListItemPress = (item) => {
+    console.log("Você clicou em:", item.nome);
+  };
+  {
+    return (
+      <View style={styles.container}>
+        <View style={styles.listContainer}>{renderJsonData()}</View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -21,16 +56,36 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#2B7C7D'
+    fontWeight: "bold",
+    color: "#2B7C7D",
   },
   positionButton: {
-    position: 'absolute',
-    bottom: 100
-  }
+    position: "absolute",
+    bottom: 100,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  listContainer: {
+    marginTop: 50,
+    width: "80%",
+  },
+  itemContainer: {
+    backgroundColor: "#000000",
+    borderBottomWidth: 1,
+    borderBottomColor: "#FFFFFF",
+    padding: 20,
+  },
+  itemText: {
+    fontSize: 18,
+    color: "#FFFFFF",
+  },
 });
