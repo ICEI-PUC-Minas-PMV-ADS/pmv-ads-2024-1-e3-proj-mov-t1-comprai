97048950325ed7f9cbba7cd4224254f3071257c1
Allan Cesar <allancagomes@hotmail.com>
2024-05-22 22:43:46 -0300

    Update home styles

--- src/App-Comprai/src/pages/Home.js
+++ src/App-Comprai/src/pages/Home.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import { useUser, } from "../contexts/UseContexts";
 
 export default function Home() {
-
+  const {name} = useUser()  
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-      <Text style={styles.mainText}> Bem Vindo ao App Compraí </Text>
+      <Text style={styles.mainText}> Bem Vindo {name}, ao App Compraí </Text>
       <Text style={styles.mainP}> Bora começar a organizar suas compras de supermercados? </Text>
       <Image style={styles.mainLogo} source={require('../../assets/eco-market--leisure-food-eco-market.png')}/>
 
@@ -35,6 +36,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   mainP:{
+    marginLeft: 15,
+    marginRight: 15,
     textAlign: 'center',
     paddingTop: 31,
     fontWeight: 'bold'
