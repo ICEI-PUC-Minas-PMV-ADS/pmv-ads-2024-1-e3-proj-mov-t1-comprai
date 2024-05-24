874a8d251f6261f9836e8afa5a3a9ff11fdf2e75
Philippe Mesquita <philippermesquita@gmail.com>
2024-05-24 18:36:22 -0300

    Update Home.js

--- src/App-Comprai/src/pages/Home.js
+++ src/App-Comprai/src/pages/Home.js
@@ -1,15 +1,14 @@
-import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import { useUser, } from "../contexts/UseContexts";
 
 export default function Home() {
-  const {name} = useUser()  
+  const { name } = useUser()
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-      <Text style={styles.mainText}> Bem Vindo {name}, ao App Compraí </Text>
-      <Text style={styles.mainP}> Bora começar a organizar suas compras de supermercados? </Text>
-      <Image style={styles.mainLogo} source={require('../../assets/eco-market--leisure-food-eco-market.png')}/>
-
+        <Text style={styles.mainText}> Bem Vindo ao App Compraí, {name} </Text>
+        <Text style={styles.mainP}> Bora começar a organizar suas compras de supermercados? </Text>
+        <Image style={styles.mainLogo} source={require('../../assets/eco-market--leisure-food-eco-market.png')} />
       </View>
     </SafeAreaView >
   );
@@ -22,27 +21,27 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     color: '#2B7C7D'
   },
-  mainText:{
+  mainText: {
     fontSize: 26,
     textAlign: 'center',
     color: '#2B7C7D',
     fontWeight: 'bold'
   },
-  mainP:{
+  mainP: {
     marginLeft: 15,
     marginRight: 15,
     textAlign: 'center',
     paddingTop: 31,
     fontWeight: 'bold'
   },
-  mainLogo:{
+  mainLogo: {
     marginTop: 46,
   }
 });
