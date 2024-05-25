b549f09ccbf92dd2df66571bda8f6001f51e5806
Philippe Mesquita <philippermesquita@gmail.com>
2024-05-25 11:59:01 -0300

    Remove: Removido "Stack Screen" que não tinha utilidade e um import

--- src/App-Comprai/src/navigation/Main.js
+++ src/App-Comprai/src/navigation/Main.js
@@ -1,10 +1,7 @@
 import React from "react";
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import CreateAccount from '../pages/CreateAccount';
 import List from '../pages/List';
-
 import Tabs from './Tabs';
 
 const Stack = createNativeStackNavigator();
@@ -16,9 +13,6 @@ export default function Main() {
     }}>
       <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
       <Stack.Screen name="List" component={List} options={{ headerBackTitleVisible: false, headerTitle: '', headerShadowVisible: false, headerTintColor: '#000' }} />
-      <Stack.Screen name="CreateAccount" component={CreateAccount} options={{
-        headerBackTitleVisible: false, headerTitle: '', headerShadowVisible: false, headerTintColor: '#000'
-      }} />
     </Stack.Navigator>
   );
 }
