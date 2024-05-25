9d09b37d3e8eea2c8e8e1d4ece9466949588c99a
Philippe Mesquita <philippermesquita@gmail.com>
2024-05-25 12:00:36 -0300

    Style:  Atualizado tela "List" para ficar igual ao layout padrão

--- src/App-Comprai/src/pages/List.js
+++ src/App-Comprai/src/pages/List.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
 import CustomButton from "../components/CustomButton";
 import CardItem from "../components/list/CardItem";
@@ -18,25 +18,26 @@ export default function List({ navigation, route }) {
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.title}>{nome}</Text>
-        <FlatList
-          style={styles.flatList}
-          data={lista}
-          renderItem={({ item }) => (
-            <CardItem
-              name={item.itemNome}
-              qnt={item.itemQuantidade}
-              value={f.format(item.itemPreco)}
-              selected={item.itemChecado}
-              onPress={() => {
-                item.itemChecado = !item.itemChecado
-                navigation.navigate("List", {
-                  nome: nome,
-                  listaInicial: lista,
-                })
-              }}
-            />
-          )}
-        />
+        <View style={styles.flatList}>
+          <FlatList
+            data={lista}
+            renderItem={({ item }) => (
+              <CardItem
+                name={item.itemNome}
+                qnt={item.itemQuantidade}
+                value={f.format(item.itemPreco)}
+                selected={item.itemChecado}
+                onPress={() => {
+                  item.itemChecado = !item.itemChecado
+                  navigation.navigate("List", {
+                    nome: nome,
+                    listaInicial: lista,
+                  })
+                }}
+              />
+            )}
+          />
+        </View>
         <View style={styles.totalPriceContainer}>
           <Text style={styles.totalPriceValue}>Valor total:</Text>
           <Text style={styles.totalPriceValue}>
@@ -84,20 +85,19 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "space-between",
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     color: "#2B7C7D",
-    marginBottom: 5,
+    marginBottom: 10,
   },
   flatList: {
-    height: "100%",
-    width: "85%",
+    height: '70%',
+    width: "90%"
   },
   totalPriceContainer: {
-    width: "85%",
+    width: "90%",
     flexDirection: "row",
     justifyContent: "space-between",
     paddingVertical: 40,
@@ -105,12 +105,14 @@ const styles = StyleSheet.create({
     marginTop: 5,
     borderRadius: 15,
     backgroundColor: "#F3A91E",
+    position: "absolute",
+    bottom: 10,
   },
   totalPriceValue: {
     fontSize: 20,
   },
   positionButton: {
     position: "absolute",
-    bottom: 100,
+    bottom: 90,
   },
 });
