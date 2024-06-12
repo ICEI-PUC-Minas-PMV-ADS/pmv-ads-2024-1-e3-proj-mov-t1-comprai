dca744a0af73756010980f3ef3d105defc232043
Mahavishnu Jn <mahajneesh@hotmail.com>
2024-06-12 19:43:11 -0300

    adiciona alterar titulo e services de submit lista

--- src/App-Comprai/src/pages/List.js
+++ src/App-Comprai/src/pages/List.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Alert, StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
+import { Alert, StyleSheet, Text, View, SafeAreaView, TextInput, FlatList, TouchableOpacity } from "react-native";
 import CustomButton from "../components/CustomButton";
 import CardItem from "../components/list/CardItem";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { criarLista } from "../services/Lists.services";
+import Input from "../components/Input";
 
 export default function List({ navigation, route }) {
-  const [nomeLista, setNomeLista] = useState('Aleatorio1');
+  const [nomeLista, setNomeLista] = useState('Titulo');
   const { listaInicial } = route.params;
   const [lista, setLista] = useState(listaInicial || []);
   const [idUser, setIdUser] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [newNomeLista, setNewNomeLista] = useState(nomeLista);
 
   let calculeTotalPrice = (item) => {
     let totalPrice = 0;
@@ -62,17 +65,37 @@ export default function List({ navigation, route }) {
 
   const handleSubmitLista = async () => {
     try {
-      await criarLista(lista, nomeLista, idUser);
+      await criarLista(nomeLista, idUser, lista);
       Alert.alert('Sucesso', 'Lista salva com sucesso!');
     } catch {
       Alert.alert('Erro', 'Não foi possível salvar a lista.');
     }
   }
 
+  const handleTextPress = () => {
+    setIsEditing(true);
+  };
+
+  const handleInputBlur = () => {
+    setNomeLista(newNomeLista);
+    setIsEditing(false);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.title}>{nomeLista}</Text>
+        {isEditing ? (
+          <TextInput
+            style={styles.input}
+            value={newNomeLista}
+            onChangeText={setNewNomeLista}
+            onBlur={handleInputBlur}
+            autoFocus
+          />
+        ) : (
+          <TouchableOpacity onPress={handleTextPress}>
+            <Text style={styles.title}>{nomeLista}</Text>
+          </TouchableOpacity>
+        )}
         <View >
           <CustomButton
             title={"Salvar Lista"}
@@ -135,6 +158,13 @@ const styles = StyleSheet.create({
   flatList: {
     height: '70%',
     width: "90%"
+  },
+  input: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    borderBottomWidth: 3,
+    borderBottomColor: '#262424',
   },
   totalPriceContainer: {
     width: "90%",
